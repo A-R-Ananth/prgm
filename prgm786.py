d689527a77723a74de6d91bd074e4eeb01689546
A-R-Ananth <anand.raju053@gmail.com>
2019-07-03 10:52:25 +0530

    Update and rename prgm3.py to prgm786.py

---
+++ prgm786.py
@@ -0,0 +1,10 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+number3 = float(input("Enter third number: "))
+if(number1>number2)and(number1>number3):
+   print(largest = number1)
+elif (number2>number1)and(number2>number3):
+   print(largest = number2)
+else:
+   print(largest = number3)
+ print("The largest number is",largest)
